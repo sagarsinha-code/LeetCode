1d64bdc063a4786aa188ee0161dd960ddad704e3
sagarsinha-code <58719504+sagarsinha-code@users.noreply.github.com>
2024-06-12 10:51:03 +0530

    Time: 119 ms (20.21%) | Memory: 49.1 MB (91.63%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,16 +4,6 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // const obj = {};
-    // for(let i = 0; i<nums.length; i++) {
-    //     console.log(obj[target - nums[i]])
-    //     if(obj[target - nums[i]] !== undefined){
-    //         return [obj[target - nums[i]], i];
-    //     }else{
-    //         obj[nums[i]] = i;
-    //     }
-    // }
-
     for(let i=0; i<nums.length; i++) {
     const fVal = target - nums[i];
     if(nums.includes(fVal) && nums.indexOf(fVal) !== i) {
