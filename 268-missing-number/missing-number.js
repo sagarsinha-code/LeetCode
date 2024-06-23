d3fa2d6eb906c9e7d246de9d413ad8793e5646fa
sagarsinha-code <58719504+sagarsinha-code@users.noreply.github.com>
2024-06-24 03:39:22 +0530

    Time: 76 ms (24.56%) | Memory: 51.4 MB (49.10%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -3,12 +3,15 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    const n = nums.length;
-    let missingNum = 0;
-    let sum = 0;
-    for(let i=0; i<nums.length; i++) {
-        sum = sum + nums[i];
+    const sortedArr = nums.sort((a,b) => a - b);
+  let output = 0;
+  for(let i=0; i<=sortedArr.length; i++) {
+    if(!sortedArr.includes(0)) {
+      return output;
     }
-    missingNum = (n * (n + 1) / 2);
-    return missingNum - sum;
+    if(sortedArr[i+1] - sortedArr[i] > 1 || sortedArr[i+1] === undefined) {
+      output = sortedArr[i] + 1;
+      return output;
+    }
+  }
 };
