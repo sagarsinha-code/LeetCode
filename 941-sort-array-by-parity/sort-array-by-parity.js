bf182086b5d6c51a3a2d86b3b2b30072d6337254
sagarsinha-code <58719504+sagarsinha-code@users.noreply.github.com>
2024-07-12 23:06:12 +0530

    Time: 63 ms (72.59%) | Memory: 51.3 MB (94.79%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.js
+++ 941-sort-array-by-parity/sort-array-by-parity.js
@@ -3,22 +3,15 @@
  * @return {number[]}
  */
 var sortArrayByParity = function(nums) {
-    // let i =0;
-    // let j =1;
+    let i=0;
 
-    // while(j==nums.length) {
-    //     if(num[i] % 2 === 0) {
-
-    //     }
-    // }
-const even = [];
-const odd = [];
-    for(let i = 0; i<nums.length; i++) {
-        if(nums[i]%2 === 0) {
-            even.push(nums[i]);
-        } else {
-            odd.push(nums[i]);
+    for(let j=0; j<nums.length; j++) {
+        if(nums[j]%2 === 0) {
+            const temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
         }
     }
-    return [...even, ...odd]
+    return nums;
 };
