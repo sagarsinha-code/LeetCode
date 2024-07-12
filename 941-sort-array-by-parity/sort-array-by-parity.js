8dd43416d4494b023859b47ef1634273c6c0e39b
sagarsinha-code <58719504+sagarsinha-code@users.noreply.github.com>
2024-07-12 23:06:52 +0530

    Time: 65 ms (63.90%) | Memory: 52.1 MB (52.29%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.js
+++ 941-sort-array-by-parity/sort-array-by-parity.js
@@ -1,10 +1,5 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
 var sortArrayByParity = function(nums) {
     let i=0;
-
     for(let j=0; j<nums.length; j++) {
         if(nums[j]%2 === 0) {
             const temp = nums[i];
